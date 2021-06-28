db708fc1b0ea7d6e02fc5955bb6edb20503eb152
Simocaso <simonecasoni97@gmail.com>
2021-06-28 17:00:29 +0000

    added fixture and loaddata

--- memberships/admin.py
+++ memberships/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import memberships
+from .models import membership
 
 # Register your models here.
-admin.site.register(memberships)
+admin.site.register(membership)
--- memberships/models.py
+++ memberships/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class membership(models.Model):
 
     class Meta:
