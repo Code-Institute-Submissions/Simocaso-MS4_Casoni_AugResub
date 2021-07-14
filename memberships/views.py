4c236607698cbe62a8f54736fbf62f6386440603
Simocaso <simonecasoni97@gmail.com>
2021-07-14 22:58:05 +0000

    added search functionality and updated views.py

--- memberships/views.py
+++ memberships/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q
+
 from .models import Membership
 
 # Create your views here.
@@ -8,6 +11,18 @@ def all_memberships(request):
     """ A view to show all the memberships plans"""
 
     memberships = Membership.objects.all()
+    # Code used from Boutique Ado - CI Lesson
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "No search request")
+                return redirect(reverse('Memberships'))
+
+            queries = Q(name__icontains=query) | Q(
+                description__icontains=query
+                )
+            memberships = memberships.filter(queries)
 
     context = {
         'memberships': memberships,
