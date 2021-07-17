c735219e04e61c18b41834e96ffba95ea62861c9
Simocaso <simonecasoni97@gmail.com>
2021-07-17 18:35:26 +0000

    fixed products json file and applied migrations

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Product
 
 # Register your models here.
+
+# groups by column the membership table in the admin tab
+
+
+class ProductAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'name',
+        'description',
+        'price',
+        'image_url',
+        'image',
+        )
+
+
+admin.site.register(Product, ProductAdmin)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Products'
+
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=2000)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
