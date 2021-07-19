d87ae11f2b17dd95b77515e6e3b1fd040ac42db2
Simocaso <simonecasoni97@gmail.com>
2021-07-19 23:04:05 +0000

    created product page and product details page plus css

--- products/views.py
+++ products/views.py
@@ -42,4 +42,4 @@ def product_detail(request, product_id):
         'product': product,
     }
 
-    return render(request, 'products/products_detail.html', context)
+    return render(request, 'products/product_detail.html', context)
