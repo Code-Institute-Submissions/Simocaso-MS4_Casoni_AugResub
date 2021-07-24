f8cac8e53d00a6b6c8b81ebe44352373cba20b4a
Simocaso <simonecasoni97@gmail.com>
2021-07-24 01:54:57 +0000

    added add_item views and urls

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -6,3 +6,20 @@ from django.shortcuts import render
 def view_bag(request):
     """ a view to see the products in the bag """
     return render(request, 'bag/bag.html')
+
+
+def add_item(request, product_id):
+    """ add products to the bag """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if product_id in list(bag.keys()):
+        bag[product_id] += quantity
+    else:
+        bag[product_id] = quantity
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
