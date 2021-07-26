8800a9baf779731344affa89fcfcc9fddd1e7193
Simocaso <simonecasoni97@gmail.com>
2021-07-26 23:01:00 +0000

    adjusted bag contexts and bag icon display in nav bar

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,4 +1,5 @@
-from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def bag_contents(request):
@@ -7,6 +8,17 @@ def bag_contents(request):
     total = 0
     product_count = 0
     delivery = 9.99
+    bag = request.session.get('bag', {})
+
+    for product_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=product_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_products.append({
+            'product_id': product_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     context = {
         'bag_products': bag_products,
