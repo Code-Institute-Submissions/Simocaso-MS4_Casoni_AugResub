b49735a77410a718bc9de5c77962430a0a89d5e6
Simocaso <simonecasoni97@gmail.com>
2021-07-29 20:56:01 +0000

    added subtotal function and restyling bag html

--- bag/contexts.py
+++ bag/contexts.py
@@ -6,6 +6,7 @@ def bag_contents(request):
 
     bag_products = []
     total = 0
+    subtotal = 0
     product_count = 0
     delivery = 9.99
     bag = request.session.get('bag', {})
@@ -13,6 +14,7 @@ def bag_contents(request):
     for product_id, quantity in bag.items():
         product = get_object_or_404(Product, pk=product_id)
         total += quantity * product.price
+        subtotal = quantity * product.price
         product_count += quantity
         bag_products.append({
             'product_id': product_id,
