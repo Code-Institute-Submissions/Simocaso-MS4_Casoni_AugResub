c7543f6fddcce5ce0e2783775426ac6b14155204
Simocaso <simonecasoni97@gmail.com>
2021-07-31 13:40:27 +0000

    updated delivery sum and bag page

--- bag/contexts.py
+++ bag/contexts.py
@@ -6,29 +6,40 @@ def bag_contents(request):
 
     bag_products = []
     total = 0
-    subtotal = {}
     product_count = 0
     delivery = 9.99
     bag = request.session.get('bag', {})
 
-    for product_id, quantity in bag.items():
-        product = get_object_or_404(Product, pk=product_id)
-        total += quantity * product.price
-        subtotal = str(quantity * product.price)
-        product_count += quantity
-        bag_products.append({
-            'product_id': product_id,
-            'quantity': quantity,
-            'product': product,
-        })
-        print("it's" + subtotal)
+    for product_id, product_data in bag.items():
+        if isinstance(product_data, int):
+            product = get_object_or_404(Product, pk=product_id)
+            total += product_data * product.price
+            product_count += product_data
+            bag_products.append({
+                'product_id': product_id,
+                'quantity': product_data,
+                'product': product,
+            })
+        else:
+            product = get_object_or_404(Product, pk=product_id)
+            for size, quantity in product_data['products_by_size'].products():
+                total += quantity * product.price
+                product_count += product_data
+                bag_products.append({
+                    'product_id': product_id,
+                    'quantity': product_data,
+                    'product': product,
+                    'size': size,
+                })
+
+    final_total = delivery + float(total)
 
     context = {
         'bag_products': bag_products,
         'total': total,
         'product_count': product_count,
         'delivery': delivery,
-        'subtotal': subtotal,
+        'final_total': final_total,
     }
 
     return context
--- bag/views.py
+++ bag/views.py
@@ -13,12 +13,24 @@ def add_item(request, product_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     bag = request.session.get('bag', {})
 
-    if product_id in list(bag.keys()):
-        bag[product_id] += quantity
+    if size:
+        if product_id in list(bag.keys()):
+            if size in bag[product_id]['products_by_size'][size].keys():
+                bag[product_id]['products_by_size'][size] += quantity
+            else:
+                bag[product_id]['products_by_size'][size] = quantity
+        else:
+            bag[product_id] = {'products_by_size': {size: quantity}}
     else:
-        bag[product_id] = quantity
+        if product_id in list(bag.keys()):
+            bag[product_id] += quantity
+        else:
+            bag[product_id] = quantity
 
     request.session['bag'] = bag
     return redirect(redirect_url)
