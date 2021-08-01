0ace62f5da0763be9d3e95e104490ec2defb6a07
Simocaso <simonecasoni97@gmail.com>
2021-08-01 21:03:54 +0000

    added toast for info, warning and error

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+
+from products.models import Product
 
 # Create your views here.
 
@@ -12,6 +15,7 @@ def view_bag(request):
 def add_item(request, product_id):
     """ add products to the bag """
 
+    product = Product.objects.get(pk=product_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
