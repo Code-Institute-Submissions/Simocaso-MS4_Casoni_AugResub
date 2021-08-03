92150241df9d3a151022fe7cc7f06c22422050a2
Simocaso <simonecasoni97@gmail.com>
2021-08-03 22:37:11 +0000

    updated bug, could not update qty in views before medium breakpoint

--- checkout/models.py
+++ checkout/models.py
@@ -4,16 +4,16 @@ from django.db import models
 class Order(models.Model):
 
     order_number = models.CharField(max_length=32, null=False, editable=False)
-    name = models.CharField(max_length=60, null=False, editable=False)
-    phone_number = models.CharField(max_length=60, null=False, editable=False)
-    email_address = models.CharField(max_length=30, null=False, editable=False)
-    address_1 = models.CharField(max_length=30, null=False, editable=False)
-    address_2 = models.CharField(max_length=30, null=False, editable=False)
-    postcode = models.CharField(max_length=30, null=False, editable=False)
+    name = models.CharField(max_length=60, null=False, blank=False)
+    phone_number = models.CharField(max_length=15, null=False, blank=False)
+    email_address = models.CharField(max_length=30, null=False, blank=False)
+    address_1 = models.CharField(max_length=80, null=False, blank=False)
+    address_2 = models.CharField(max_length=80, null=False, blank=True)
+    postcode = models.CharField(max_length=20, null=True, editable=True)
     town_or_city = models.CharField(max_length=30, null=False, editable=False)
-    county = models.CharField(max_length=30, null=False, editable=False)
-    country = models.CharField(max_length=30, null=False, editable=False)
-    date = models.CharField(max_length=30, null=False, editable=False)
+    county = models.CharField(max_length=70, null=True, blank=True)
+    country = models.CharField(max_length=30, null=False, blank=False)
+    date = models.DateTimeField(auto_now_add=True)
     delivery = models.DecimalField(
         max_digits=6, decimal_places=2, null=False, default=0)
     total = models.DecimalField(
