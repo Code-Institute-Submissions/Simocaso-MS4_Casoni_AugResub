1ff52da6c58d0e75071a479438b0d578f06f0e68
Simocaso <simonecasoni97@gmail.com>
2021-08-05 00:51:56 +0000

    added and updated checkout html file

--- motigym/urls.py
+++ motigym/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path('memberships/', include('memberships.urls')),
     path('products/', include('products.urls')),
     path('info/', include('info.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
