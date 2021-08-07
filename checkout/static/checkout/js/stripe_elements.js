1891b6d36c55028606a1dee83b20877ca78eb374
Simocaso <simonecasoni97@gmail.com>
2021-08-07 19:32:21 +0000

    updated products html page

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,8 +1,6 @@
 /*
     Core logic/payment flow for this comes from here:
-    https://stripe.com/docs/payments/accept-a-payment
-    CSS from here: 
-    https://stripe.com/docs/stripe-js
+    https://stripe.com/docs/js
 */
 
 var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
