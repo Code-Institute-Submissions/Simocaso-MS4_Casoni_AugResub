9f7d57f92ce69031cb6425ec7de50e1e003b5f41
Simocaso <simonecasoni97@gmail.com>
2021-08-08 07:59:37 +0000

    added views py profiles

--- checkout/models.py
+++ checkout/models.py
@@ -6,12 +6,17 @@ from django.db.models import Sum
 from django_countries.fields import CountryField
 
 from products.models import Product
+from profiles.models import UserProfile
 
 # Code partly used from CI checkout lesson
 
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile,
+                                     on_delete=models.SET_NULL,
+                                     null=True, blank=True,
+                                     related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
@@ -25,16 +30,10 @@ class Order(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     delivery_cost = models.DecimalField(
         max_digits=6, decimal_places=2, null=False, default=9.99)
-    order_total = models.DecimalField(
-        max_digits=10,
-        decimal_places=2,
-        null=False,
-        default=0)
-    final_total = models.DecimalField(
-        max_digits=10,
-        decimal_places=2,
-        null=False,
-        default=0)
+    order_total = models.DecimalField(max_digits=10,decimal_places=2,
+                                      null=False, default=0)
+    final_total = models.DecimalField(max_digits=10, decimal_places=2,
+                                      null=False, default=0)
     original_bag = models.TextField(null=False, blank=False, default='')
     stripe_pid = models.CharField(
         max_length=254, null=False, blank=False, default='')
