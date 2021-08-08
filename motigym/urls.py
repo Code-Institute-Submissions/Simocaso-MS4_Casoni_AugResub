5a9b0ff96bc9c9f14f966592e226d3ae84bcfa08
Simocaso <simonecasoni97@gmail.com>
2021-08-08 08:26:56 +0000

    created and tested profile html page

--- motigym/urls.py
+++ motigym/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('info/', include('info.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- profiles/urls.py
+++ profiles/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
         '',
         views.profile,
         name='profile'),
-    path(
-        'order_history/<order_number>',
-        views.order_history,
-        name='order_history'),
+    # path(
+    #     'order_history/<order_number>',
+    #     views.order_history,
+    #     name='order_history'),
 ]
