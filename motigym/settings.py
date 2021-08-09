f3aa3781de0df3716849db4c72e5bcee8ffbbb3f
Simocaso <simonecasoni97@gmail.com>
2021-08-09 02:51:04 +0000

    deleted the app

--- motigym/settings.py
+++ motigym/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = 'django-insecure-$77jo%4g5l(ri_&*6_barb_9h@!q&7wc%)zy^n5!t@oslgljl+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENTE' in os.environ
+DEBUG = True
 
-ALLOWED_HOSTS = ['https://moti-gym.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -52,7 +52,6 @@ INSTALLED_APPS = [
     'django_countries',
     'profiles',
     'crispy_forms',
-    'storages',
 ]
 
 MIDDLEWARE = [
@@ -130,15 +129,11 @@ WSGI_APPLICATION = 'motigym.wsgi.application'
 #         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 #     }
 # else:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#         }
-#     }
-
 DATABASES = {
-    'default': dj_database_url.parse('postgres://kehcarqhwjyhjr:1dbc711221c288664432c360c7fe3d13b678ab0199aa031bd53fa73afab1708d@ec2-54-74-60-70.eu-west-1.compute.amazonaws.com:5432/d6j2v93ejvhc7n')
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
@@ -190,31 +185,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-if 'USE_AWS' in os.environ:
-    # Cache control
-    AWS_S3_OBJECT_PARAMETERS = {
-        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-        'CacheControl': 'max-age=94608000',
-    }
-
-    # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'moti-gym'
-    AWS_S3_REGION_NAME = 'eu-west-2'
-    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-
-    # Static and media files
-    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-    STATICFILES_LOCATION = 'static'
-    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
-    MEDIAFILES_LOCATION = 'media'
-
-    # Override static and media URLs in production
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
-
 
 # Stripe
 STRIPE_CURRENCY = 'eur'
