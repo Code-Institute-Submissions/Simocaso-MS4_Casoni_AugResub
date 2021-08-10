75c6f137a0adffdd36a4b7a4ab7997c13f5297db
Simocaso <simonecasoni97@gmail.com>
2021-08-10 08:39:32 +0000

    updated secret key in test

--- motigym/settings.py
+++ motigym/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+# SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = 'd5@si^2xce(uerc&q#2ohm%0=ejk5_e%7v)y!hj-*1%7+mf7pc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
